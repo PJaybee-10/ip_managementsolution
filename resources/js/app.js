7224f198524d11cd7af8921e71e3f6d66589336c
phelsajc <phelsajc@users.noreply.github.com>
2024-04-07 21:05:12 +0800

    Remove unnecessary code

--- resources/js/app.js
+++ resources/js/app.js
@@ -21,20 +21,10 @@ window.Swal = Swal;
 
 import VuePdfApp from "vue-pdf-app";
 window.VuePdfApp = VuePdfApp;
-/* Vue.component('phicModal', require('./components/modals/phicPayment.vue').default);
-Vue.component('patientComponent', require('./components/Autocomplete.vue').default);
-Vue.component('companyComponent', require('./components/Company.vue').default);
-Vue.component('productComponent', require('./components/ProductComponent.vue').default); */
 Vue.component('ipAddressModal', require('./components/modals/ipaddModal.vue').default);
-
-/* Vue.component('diagnostic', require('./components/AutocompleteD.vue').default);
-Vue.component('user-info', require('./components/User.vue').default);
-Vue.component('pxmodal', require('./components/Prescription.vue').default); */
 Vue.component('navComponent', require('./components/template/nav.vue').default);
 Vue.component('sidemenuComponent', require('./components/template/sidemenu.vue').default);
 Vue.component('footerComponent', require('./components/template/footer.vue').default);
-/* Vue.component('products', require('./components/Products.vue').default); */
-/* Vue.component('progressBar', require('./components/ProgressBar.vue').default); */
 const Toast = Swal.mixin({
     toast: true,
     position: 'top-end',
@@ -44,10 +34,6 @@ const Toast = Swal.mixin({
     showConfirmButton: false,
     timer: 60000,
     timerProgressBar: true,
-    /* didOpen: (toast) => {
-      toast.addEventListener('mouseenter', Swal.stopTimer)
-      toast.addEventListener('mouseleave', Swal.resumeTimer)
-    } */
 });
 
 window.Toast = Toast
